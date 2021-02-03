f6455a110c896e3d2c6e8f07e3e00a632dffc630
Davey van Tilburg <daveyvantilburg@gmail.com>
2021-02-03 16:17:54 +0100

    Init

---
+++ challange_data_reporting/Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace challange_data_reporting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
